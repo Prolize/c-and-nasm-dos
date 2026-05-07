041136d5fcd223c8baa5103f4412c207e56c36e8
Prolize <lokh22332233@gmail.com>
2026-05-07 08:19:24 +0300

    Create hello_world2.c

---
+++ hello_world2.c
@@ -0,0 +1,11 @@
+// It's harder, but for me... Uuum... It's easy
+
+#include <stdio.h>
+void hellowrld(){
+     printf("Hello world!");
+}
+
+int main() {
+    hellowrld(); // we're call function, because I want this
+    return 0
+}
