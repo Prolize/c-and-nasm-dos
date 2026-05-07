abb093c997b817cf9fee8989413b85ef436379c0
Prolize <lokh22332233@gmail.com>
2026-05-07 08:30:06 +0300

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,17 @@
+// Note: This is very easy calculator
+// Who cares?? 
+
+#include <stdio.h>
+// Note: I wanna use printf(), because it's calculator about print result (a + b)
+// UWU
+
+int main() {
+   int a, b;
+   scanf("%d", &a);
+   scanf("%d", &b);
+// Note: main part of our calculator
+// Yeah, I agree
+   int c = a + b;
+   printf("%d", c);
+   return 0;
+}
