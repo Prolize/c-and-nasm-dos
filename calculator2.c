5e53fa69e30eb1ec85e463678a6e618403cc2ed1
Prolize <lokh22332233@gmail.com>
2026-05-08 10:00:49 +0300

    Update calculator2.c

--- calculator2.c
+++ calculator2.c
@@ -4,5 +4,29 @@
 #include <stdio.h>
 
 int main() {
-   pritnf("1) addition 2) division 3) multiplication\n");
-   scanf
+   printf("1) addition 2) division 3) multiplication\n");
+   int a = 0;
+   scanf("%d", &a);
+   if(a == 1) {
+      float a, b;
+      scanf("%g", &a);
+      scanf("%g", &b);
+      float c = a + b;
+      printf("%g", c);
+   }
+   if(a == 2) {
+      float a, b;
+      scanf("%g", &a);
+      scanf("%g", &b);
+      float c = a / b;
+      printf("%g", c);
+   }
+    if(a == 3) {
+      float a, b;
+      scanf("%g", &a);
+      scanf("%g", &b);
+      float c = a * b;
+      printf("%g", c);
+    }
+    return 0;
+}
