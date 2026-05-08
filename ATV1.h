aab591ef3c526371c4742f787c519b637f3c6ba7
Prolize <lokh22332233@gmail.com>
2026-05-08 10:05:15 +0300

    Create ATV1.h

---
+++ ATV1.h
@@ -0,0 +1,35 @@
+// Atv header for working with ATV (amateur television)
+// Заголовок АТВ для работы с конкретными значениями (не нужно искать их в интернете)
+#ifndef ATV_H
+#define ATV_H
+
+static const int pal_h = 768;
+static const int pal_v = 576;
+static const int pal_m_h = 640;
+static const int pal_m_v = 480;
+static const int ntsc_h = 640;
+static const int ntsc_v = 480;
+static const int secam_h = 768;
+static const int secam_v = 576;
+static const int nbtv32_h = 32;
+static const int nbtv32_v = 32;
+static const int nbtv32_43_h = 43;
+static const int nbtv32_43_v = 32;
+static const int nbtv64_h = 64;
+static const int nbtv64_v = 64;
+static const int nbtv64_43_h = 85;
+static const int nbtv64_43_v = 64;
+static const int nbtv120_h   = 120;
+static const int nbtv120_v   = 120;
+static const int nbtv120_43_h = 160;
+static const int nbtv120_43_v = 120;
+static const int nbtv128_h   = 128;
+static const int nbtv128_v   = 128;
+static const int nbtv128_43_h = 170;
+static const int nbtv128_43_v = 128;
+static const int nbtv240_h   = 240;
+static const int nbtv240_v   = 240;
+static const int nbtv240_43_h = 320;
+static const int nbtv240_43_v = 240;
+
+#endif
